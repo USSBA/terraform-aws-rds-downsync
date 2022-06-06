9067b8f2e83f476bab3d5d03cf3dd14b3775e642
Tyler Webb <twebb@sba.gov>
2022-06-06 12:15:49 -0400

    IA-2404: Adjusted entrypoint to support SQL scrub

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
 
-echo "Hello IAS"
+# lets use postgres 12 by default
+export SQL_CLIENT=${SQL_CLIENT:-postgresql-client-12}
+
+echo "Updaing Packages"
+apt-get update
+
+case $SQL_CLIENT in
+  postgresql*)
+    apt-get install -y $SQL_CLIENT
+    export DUMP="pg_dump --verbose --format c -f"
+    export RESTORE="pg_restore --verbose"
+    export SQL="psql -f"
+  ;;
+  mysql*)
+    apt-get install -y $SQL_CLIENT
+    export DUMP="undefined"
+    export RESTORE="undefined"
+    export SQL="undefined"
+  ;;
+  *)
+    echo "The SQL_CLIENT is not supported"
+    echo "Supported Clients are [postgresql-client-10 | postgresql-client-11 | postgresql-client-12 | mysql-client]"
+    exit 1
+  ;;
+esac
+
+mkdir -p ${DUMP_DIR}
+cd ${DUMP_DIR}
+
+exec "$@"
